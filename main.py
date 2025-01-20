14c36efbcbcd95da39b1c626b2133e4a8c3b5f84
TB <tlb.python@gmail.com>
2025-01-20 15:35:53 -0500

    Add description to columns - day21

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
+st.set_page_config(layout='wide')
+
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/taz.png")
+    st.image("images/photo.png")
 
 with col2:
     st.title("Tracy")
@@ -11,3 +13,7 @@ with col2:
     Hello , I am learning more python today """
 
     st.info(content)
+
+
+content2 = """Below are soem apps I created """
+st.write(content2)
