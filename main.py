7bdb74538eba29250d917ce1cc2d04829920c0c5
TB <tlb.python@gmail.com>
2025-01-20 16:46:48 -0500

    description images and links - adjust column

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/taz.png")
 
 with col2:
     st.title("Tracy")
@@ -19,17 +19,24 @@ with col2:
 content2 = """Below are some apps I created - Feel free to contact me! """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empy_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
 
