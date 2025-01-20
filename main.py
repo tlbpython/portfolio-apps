7f396bbc2bf09b560e375fc0ba04525f3ef5947a
TB <tlb.python@gmail.com>
2025-01-20 16:07:18 -0500

    day22 - render columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -15,5 +16,21 @@ with col2:
     st.info(content)
 
 
-content2 = """Below are soem apps I created """
+content2 = """Below are some apps I created - Feel free to contact me! """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
