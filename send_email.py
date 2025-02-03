a76119bc97a7260de23ccc5aa1a39782bb595832
TB <tlb.python@gmail.com>
2025-02-03 15:56:51 -0500

    Send email

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "tlb.python@gmail.com"
+password = "qobghdhymgyyaotl"
+
+receiver = "tlbutler007@gmail.com"
+context = ssl.create_default_context()
+
+message = """\
+Subject: Hi!  
+How are you?  Goodbye!"""
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
+
+#on mac ssl error - go to directory for python and click "install certificates"
