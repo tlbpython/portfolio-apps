a53cff1fb2d5029b4cda5597bb2f1787cbd368a8
TB <tlb.python@gmail.com>
2025-02-03 16:16:56 -0500

    Send email using form

--- send_email.py
+++ send_email.py
@@ -1,20 +1,19 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "tlb.python@gmail.com"
-password = "qobghdhymgyyaotl"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "tlbutler007@gmail.com"
-context = ssl.create_default_context()
+    username = "tlb.python@gmail.com"
+    password = "qobghdhymgyyaotl"
 
-message = """\
-Subject: Hi!  
-How are you?  Goodbye!"""
+    receiver = "tlbutler007@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 #on mac ssl error - go to directory for python and click "install certificates"
